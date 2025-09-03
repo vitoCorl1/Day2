36261d0d1a5dad3d66328fb6fa11062cf773831b
Vito Corl <elhkadrawiaymane@gmail.com>
2025-09-03 11:30:53 +0100

    program that multiplies each element of an integer array by a given factor and displays the resulting array.

---
+++ arrays/Multiplying_Elements.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int n_elements, fact;
+    int elements[n_elements];
+    printf("the number of elements : ");
+    scanf("%d", &n_elements);
+    printf("the elements : ");
+    for(int i = 0; i < n_elements; i++){
+        scanf("%d", &elements[i]);
+    }
+    printf("the multiplication factor : ");
+    scanf("%d", &fact);
+    int fact_r[n_elements];
+    for(int i = 0; i < n_elements; i++){
+        fact_r[i] = elements[i] * fact;
+        // printf("resulting arr :");
+        printf("resulting arr : %d\n", fact_r[i]);
+    }
+}
